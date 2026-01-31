653c84ed915e7b517b0a77586596603d73cfaa14
Floris de Bijl <floris.debijl@gmail.com>
2026-01-31 15:59:05 +0100

    Point main menu to game scene

--- menus/MainMenu.cs
+++ menus/MainMenu.cs
@@ -2,19 +2,27 @@ using Godot;
 
 public partial class MainMenu : Control {
   [Export] private PackedScene gameScene;
+  [Export] private PackedScene demoScene;
   [Export] private Button startButton;
+  [Export] private Button demoButton;
   [Export] private Button exitButton;
   [Export] private AudioStream bgmStart;
 
   public override void _Ready() {
+    // Re-enable once we have bgm
     // AudioManager.PlaySFX(bgmStart, 1, false, GlobalPosition);
     startButton.ButtonDown += OnStartButtonPressed;
+    demoButton.ButtonDown += OnDemoButtonPressed;
     exitButton.ButtonDown += OnExitButtonPressed;
     startButton.GrabFocus();
   }
 
   private void OnStartButtonPressed() {
     GetTree().ChangeSceneToPacked(gameScene);
+  }
+
+  private void OnDemoButtonPressed() {
+    GetTree().ChangeSceneToPacked(demoScene);
   }
 
   private void OnExitButtonPressed() {
@@ -25,6 +33,8 @@ public partial class MainMenu : Control {
     if (@event is InputEventJoypadButton joypadEvent) {
       if (joypadEvent.ButtonIndex == JoyButton.A && joypadEvent.Pressed && startButton.HasFocus()) {
         OnStartButtonPressed();
+      } else if (joypadEvent.ButtonIndex == JoyButton.A && joypadEvent.Pressed && demoButton.HasFocus()) {
+        OnDemoButtonPressed();
       } else if (joypadEvent.ButtonIndex == JoyButton.A && joypadEvent.Pressed && exitButton.HasFocus()) {
         OnExitButtonPressed();
       }
