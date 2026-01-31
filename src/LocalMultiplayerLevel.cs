eafeb6e04137fbc12dfc38dc2096111dbe871b9a
Felipe Menelau <felip3menelau2@gmail.com>
2026-01-31 11:50:47 +0100

    feat: naive goal zone implementation

--- src/LocalMultiplayerLevel.cs
+++ src/LocalMultiplayerLevel.cs
@@ -17,18 +17,18 @@ public partial class LocalMultiplayerLevel : Node
 
   public Dictionary<int, Node> JoypadToPlayer = new();
 
-	public Node KBPlayer = null;
+  public Node KBPlayer = null;
 
-	public override void _Input(InputEvent @event)
-	{
-		// Handle potential new joypad player
-		if (@event is InputEventJoypadButton joypadEvent)
-		{
-			// Not start
-			if (joypadEvent.ButtonIndex != JoyButton.Start)
-			{
-				return;
-			}
+  public override void _Input(InputEvent @event)
+  {
+    // Handle potential new joypad player
+    if (@event is InputEventJoypadButton joypadEvent)
+    {
+      // Not start
+      if (joypadEvent.ButtonIndex != JoyButton.Start)
+      {
+        return;
+      }
 
       // Already assigned
       if (JoypadToPlayer.ContainsKey(joypadEvent.Device))
@@ -36,52 +36,52 @@ public partial class LocalMultiplayerLevel : Node
         return;
       }
 
-			var player = AddPlayer(joypadEvent.Device);
-			JoypadToPlayer[joypadEvent.Device] = player;
-		}
+      var player = AddPlayer(joypadEvent.Device);
+      JoypadToPlayer[joypadEvent.Device] = player;
+    }
 
-		if (@event is InputEventKey keyEvent)
-		{
-			// Not escape
-			if (keyEvent.Keycode != Key.Enter)
-			{
-				return;
-			}
+    if (@event is InputEventKey keyEvent)
+    {
+      // Not escape
+      if (keyEvent.Keycode != Key.Enter)
+      {
+        return;
+      }
 
-			// Already assigned
-			if (KBPlayer != null)
-			{
-				return;
-			}
+      // Already assigned
+      if (KBPlayer != null)
+      {
+        return;
+      }
 
-			var player = AddPlayer(keyEvent.Device, true);
-			KBPlayer = player;
-		}
-	}
+      var player = AddPlayer(keyEvent.Device, true);
+      KBPlayer = player;
+    }
+  }
 
-	private Node AddPlayer(int deviceId, bool isKB = false)
-	{
-		var player = PlayerScene.Instantiate() as Player;
-		player.PlayerController.DeviceId = deviceId;
-		player.PlayerController.IsKB = isKB;
-		player.Position = SpawnLocation.Position;
-		PlayerContainer.AddChild(player, true);
-		return player;
-	}
+  private Node AddPlayer(int deviceId, bool isKB = false)
+  {
+    var player = PlayerScene.Instantiate() as Player;
+    player.PlayerController.DeviceId = deviceId;
+    player.PlayerController.IsKB = isKB;
+    player.Position = SpawnLocation.Position;
+    PlayerContainer.AddChild(player, true);
+    return player;
+  }
 
-	private void RemovePlayer(int id, bool isKB = false)
-	{
-		// Handle KB removal
-		if (isKB)
-		{
-			PlayerContainer.RemoveChild(KBPlayer);
-			KBPlayer = null;
-			return;
-		}
+  private void RemovePlayer(int id, bool isKB = false)
+  {
+    // Handle KB removal
+    if (isKB)
+    {
+      PlayerContainer.RemoveChild(KBPlayer);
+      KBPlayer = null;
+      return;
+    }
 
-		// Handle joypad removal
-		if (!JoypadToPlayer.ContainsKey(id)) return;
-		var player = JoypadToPlayer[id];
-		PlayerContainer.RemoveChild(player);
-	}
+    // Handle joypad removal
+    if (!JoypadToPlayer.ContainsKey(id)) return;
+    var player = JoypadToPlayer[id];
+    PlayerContainer.RemoveChild(player);
+  }
 }
