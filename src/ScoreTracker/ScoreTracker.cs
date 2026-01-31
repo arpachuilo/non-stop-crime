26482884f0ee0fe2be08fe191cb9a59e1b55182d
Felipe Menelau <felip3menelau2@gmail.com>
2026-01-31 13:30:16 +0100

    feat: first timer implementation

--- src/ScoreTracker/ScoreTracker.cs
+++ src/ScoreTracker/ScoreTracker.cs
@@ -5,12 +5,44 @@ using System.Linq;
 public partial class ScoreTracker : CanvasLayer
 {
 	[Export] public VBoxContainer ScoreContainer;
+	[Export] public Label TimerLabel;
+	[Export] public Label WinnerLabel;
+	[Export] public float GameDurationSeconds = 600f; // 10 minutes default
 
 	private Dictionary<Player, Label> _playerLabels = new();
+	private float _timeRemaining;
+	private bool _timerStarted = false;
+	private bool _gameEnded = false;
+
+	public override void _Ready()
+	{
+		_timeRemaining = GameDurationSeconds;
+		WinnerLabel.Visible = false;
+		UpdateTimerDisplay();
+	}
 
 	public override void _Process(double delta)
 	{
 		var players = GetTree().GetNodesInGroup(Group.Player).Cast<Player>().ToList();
+
+		// Start timer when first player spawns
+		if (!_timerStarted && players.Count > 0)
+		{
+			_timerStarted = true;
+		}
+
+		// Countdown logic
+		if (_timerStarted && !_gameEnded)
+		{
+			_timeRemaining -= (float)delta;
+			if (_timeRemaining <= 0)
+			{
+				_timeRemaining = 0;
+				_gameEnded = true;
+				DeclareWinner(players);
+			}
+			UpdateTimerDisplay();
+		}
 
 		// Add labels for new players
 		foreach (var player in players)
@@ -33,5 +65,36 @@ public partial class ScoreTracker : CanvasLayer
 			_playerLabels[player].QueueFree();
 			_playerLabels.Remove(player);
 		}
+	}
+
+	private void UpdateTimerDisplay()
+	{
+		int minutes = (int)(_timeRemaining / 60);
+		int seconds = (int)(_timeRemaining % 60);
+		TimerLabel.Text = $"{minutes:D2}:{seconds:D2}";
+	}
+
+	private void DeclareWinner(List<Player> players)
+	{
+		if (players.Count == 0)
+		{
+			WinnerLabel.Text = "No players!";
+		}
+		else
+		{
+			var winner = players.OrderByDescending(p => p.Score).First();
+			var topScore = winner.Score;
+			var tied = players.Where(p => p.Score == topScore).ToList();
+
+			if (tied.Count > 1)
+			{
+				WinnerLabel.Text = "TIE!";
+			}
+			else
+			{
+				WinnerLabel.Text = $"Winner: {winner.NamePlate.Text}!";
+			}
+		}
+		WinnerLabel.Visible = true;
 	}
 }
