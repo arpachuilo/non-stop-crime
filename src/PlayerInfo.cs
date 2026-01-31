9c381f5c1e5ebaf9a84047695a6772365ee1d3bc
Felipe Menelau <felip3menelau2@gmail.com>
2026-01-31 22:52:32 +0100

    show mask on player UI once it's picked up (ugly)

--- src/PlayerInfo.cs
+++ src/PlayerInfo.cs
@@ -10,14 +10,28 @@ public partial class PlayerInfo : Control {
   [Export]
   public Label ScoreOrReadyStatus { get; set; }
 
+  [Export]
+  public TextureRect MaskIcon { get; set; }
+
   public bool IsPlaying = false;
 
   public bool _isReady = false;
   public bool IsReady {
-	get => _isReady;
-	set {
-	  _isReady = value;
-	  ScoreOrReadyStatus.Text = _isReady ? "Ready" : "Not Ready";
-	}
+  get => _isReady;
+  set {
+    _isReady = value;
+    ScoreOrReadyStatus.Text = _isReady ? "Ready" : "Not Ready";
+  }
+  }
+
+  public void UpdateMaskIcon(Texture2D icon) {
+    if (MaskIcon == null) return;
+
+    if (icon != null) {
+      MaskIcon.Texture = icon;
+      MaskIcon.Visible = true;
+    } else {
+      MaskIcon.Visible = false;
+    }
   }
 }
