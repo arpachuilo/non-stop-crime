aeefc00a01bf9cd9d27f8233528e6f8efdcde0b5
arpachuilo <andrew.pachuilo@gmail.com>
2026-01-31 12:53:44 +0100

    Set player name on AddPlayer

--- src/LocalMultiplayerLevel.cs
+++ src/LocalMultiplayerLevel.cs
@@ -1,87 +1,94 @@
 using Godot;
 using System.Collections.Generic;
+using System.Linq;
 
 /// <summary>
 /// Handle spawning in players for level based joypad
 /// </summary>
 public partial class LocalMultiplayerLevel : Node
 {
-  [Export]
-  public PackedScene PlayerScene;
+	[Export]
+	public PackedScene PlayerScene;
 
-  [Export]
-  public Node3D SpawnLocation;
+	[Export]
+	public Node3D SpawnLocation;
 
-  [Export]
-  public Node3D PlayerContainer;
+	[Export]
+	public Node3D PlayerContainer;
 
-  public Dictionary<int, Node> JoypadToPlayer = new();
+	public Dictionary<int, Player> JoypadToPlayer = new();
 
-  public Node KBPlayer = null;
+	public Player KBPlayer = null;
 
-  public override void _Input(InputEvent @event)
-  {
-    // Handle potential new joypad player
-    if (@event is InputEventJoypadButton joypadEvent)
-    {
-      // Not start
-      if (joypadEvent.ButtonIndex != JoyButton.Start)
-      {
-        return;
-      }
+	public List<Player> Players
+	{
+		get => [.. JoypadToPlayer.Values, .. KBPlayer != null ? new[] { KBPlayer } : []];
+	}
 
-      // Already assigned
-      if (JoypadToPlayer.ContainsKey(joypadEvent.Device))
-      {
-        return;
-      }
+	public override void _Input(InputEvent @event)
+	{
+		// Handle potential new joypad player
+		if (@event is InputEventJoypadButton joypadEvent)
+		{
+			// Not start
+			if (joypadEvent.ButtonIndex != JoyButton.Start)
+			{
+				return;
+			}
 
-      var player = AddPlayer(joypadEvent.Device);
-      JoypadToPlayer[joypadEvent.Device] = player;
-    }
+			// Already assigned
+			if (JoypadToPlayer.ContainsKey(joypadEvent.Device))
+			{
+				return;
+			}
 
-    if (@event is InputEventKey keyEvent)
-    {
-      // Not escape
-      if (keyEvent.Keycode != Key.Enter)
-      {
-        return;
-      }
+			var player = AddPlayer(joypadEvent.Device);
+			JoypadToPlayer[joypadEvent.Device] = player;
+		}
 
-      // Already assigned
-      if (KBPlayer != null)
-      {
-        return;
-      }
+		if (@event is InputEventKey keyEvent)
+		{
+			// Not escape
+			if (keyEvent.Keycode != Key.Enter)
+			{
+				return;
+			}
 
-      var player = AddPlayer(keyEvent.Device, true);
-      KBPlayer = player;
-    }
-  }
+			// Already assigned
+			if (KBPlayer != null)
+			{
+				return;
+			}
 
-  private Node AddPlayer(int deviceId, bool isKB = false)
-  {
-    var player = PlayerScene.Instantiate() as Player;
-    player.PlayerController.DeviceId = deviceId;
-    player.PlayerController.IsKB = isKB;
-    player.Position = SpawnLocation.Position;
-    PlayerContainer.AddChild(player, true);
-    return player;
-  }
+			var player = AddPlayer(keyEvent.Device, true);
+			KBPlayer = player;
+		}
+	}
 
-  private void RemovePlayer(int id, bool isKB = false)
-  {
-    // Handle KB removal
-    if (isKB)
-    {
-      PlayerContainer.RemoveChild(KBPlayer);
-      KBPlayer = null;
-      return;
-    }
+	private Player AddPlayer(int deviceId, bool isKB = false)
+	{
+		var player = PlayerScene.Instantiate() as Player;
+		player.PlayerController.DeviceId = deviceId;
+		player.PlayerController.IsKB = isKB;
+		player.Position = SpawnLocation.Position;
+		player.NamePlate.Text = RandomUtil.FromList(Canned.PlayerNames.Except(Players.Select(p => p.NamePlate.Text)));
+		PlayerContainer.AddChild(player, true);
+		return player;
+	}
 
-    // Handle joypad removal
-    if (!JoypadToPlayer.ContainsKey(id)) return;
-    var player = JoypadToPlayer[id];
-    PlayerContainer.RemoveChild(player);
-  }
+	private void RemovePlayer(int id, bool isKB = false)
+	{
+		// Handle KB removal
+		if (isKB)
+		{
+			PlayerContainer.RemoveChild(KBPlayer);
+			KBPlayer = null;
+			return;
+		}
+
+		// Handle joypad removal
+		if (!JoypadToPlayer.ContainsKey(id)) return;
+		var player = JoypadToPlayer[id];
+		PlayerContainer.RemoveChild(player);
+	}
 }
