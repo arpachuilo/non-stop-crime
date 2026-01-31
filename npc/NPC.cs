cfe10c78e00c184864fdfd9771eec067c3da8ee4
arpachuilo <andrew.pachuilo@gmail.com>
2026-01-31 16:54:29 +0100

    Make NPCs run from player

--- npc/NPC.cs
+++ npc/NPC.cs
@@ -1,4 +1,5 @@
 using Godot;
+using System.Linq;
 
 public partial class NPC : Character
 {
@@ -8,8 +9,34 @@ public partial class NPC : Character
   [Export]
   public string NPCName = "NPC";
 
+  [Export]
+  public float AggroRadius = 10f;
+
   public override void _Ready()
   {
     SpriteParent ??= GetNode<SpriteParent>("SpriteParent");
+  }
+
+  public override Vector3 GetDirection()
+  {
+    var players = GetTree().GetNodesInGroup(Group.Player).Cast<Player>().ToList();
+    Player closest = null;
+    var minRadius = AggroRadius * 2f;
+    foreach (var player in players)
+    {
+      var distance = GlobalPosition.DistanceTo(player.GlobalPosition);
+      if (distance < AggroRadius && distance < minRadius)
+      {
+        closest = player;
+        minRadius = distance;
+      }
+    }
+
+    if (closest != null)
+    {
+      return -(closest.GlobalPosition - GlobalPosition).Normalized();
+    }
+
+    return Vector3.Zero;
   }
 }
