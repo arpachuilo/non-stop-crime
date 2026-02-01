9218783b32b918501e8d5b781f223caa8c9e2d83
Floris de Bijl <floris.debijl@gmail.com>
2026-02-01 13:59:29 +0100

    Add small offset to mask bobbing to desync them

--- src/Mask/MaskSpriteBob.cs
+++ src/Mask/MaskSpriteBob.cs
@@ -7,18 +7,24 @@ public partial class MaskSpriteBob : Sprite3D {
 
   private Vector3 _basePosition;
   private float _time;
+  private float _offsetRotate;
+  private float _offsetBob;
 
   public override void _Ready() {
     _basePosition = Position;
+
+    // Random offset so not all the masks are bobbing in sync
+    _offsetRotate = (0.5f - GD.Randf()) / 10;
+    _offsetBob = (0.5f - GD.Randf()) / 10;
   }
 
   public override void _Process(double delta) {
     float dt = (float)delta;
     _time += dt;
 
-    RotateY(RotationSpeed * dt);
+    RotateY((RotationSpeed + _offsetRotate) * dt);
 
-    float bobOffset = Mathf.Sin(_time * Mathf.Tau * BobFrequency) * BobAmplitude;
+    float bobOffset = Mathf.Sin(_time * Mathf.Tau * (BobFrequency + _offsetBob)) * BobAmplitude;
     Position = _basePosition + Vector3.Up * bobOffset;
   }
 }
