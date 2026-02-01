d726285bb961bef67eef46be6a2a85af579754ea
arpachuilo <andrew.pachuilo@gmail.com>
2026-02-01 12:30:18 +0100

    Better bomb

--- projectiles/bomb/BOMB.cs
+++ projectiles/bomb/BOMB.cs
@@ -4,7 +4,7 @@ public partial class BOMB : Sprite3D {
   private float _timer = 0f;
 
   public override void _Process(double delta) {
-    _timer += (float)delta;
+    _timer += (float)delta * 1.9f;
     SetInstanceShaderParameter("scale", Mathf.Clamp(_timer, 0.0f, 1.0f));
   }
 }
